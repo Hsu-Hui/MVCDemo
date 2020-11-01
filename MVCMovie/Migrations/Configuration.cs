4d305a77fe7dd48b80fa9edcbe1e731d32878805
Hsuhui <la10120818@yahoo.com.tw>
2020-11-01 22:47:28 +0800

    2020/11/01
    電影清單Create、Insert、Delete、Details

---
+++ MVCMovie/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace MVCMovie.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MVCMovie.Models.MovieDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "MVCMovie.Models.MovieDBContext";
+        }
+
+        protected override void Seed(MVCMovie.Models.MovieDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- MVCMovie/Models/Movie.cs
+++ MVCMovie/Models/Movie.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVCMovie.Models
 {
     public class Movie
     {
         public int ID { get; set; }
+        [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+        [StringLength(5)]
+        public string Rating { get; set; }
+
     }
     public class MovieDBContext : DbContext
     {
